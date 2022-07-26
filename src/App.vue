6389164824dcd031b076e8375e87072860bf4e97
salamat9 <salamatbaichoroev@gmail.com>
2022-07-27 01:18:09 +0600

    add color mode and TodoPage views

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
-<script setup>
-</script>
-
 <template>
-  <h1>Hello, Essa</h1>
+	<div>
+		<h1>Hello, Essa</h1>
+		<TodoList />
+		<button @click="toggleDark"></button>
+	</div>
 </template>
+
+<script setup>
+import TodoList from '@/views/TodoPage/Index.vue';
+</script>
 
 <style scoped>
 </style>
