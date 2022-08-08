0acdfe1529b26d970d6533d7b8739a06c13a3ef0
salamat9 <salamatbaichoroev@gmail.com>
2022-08-08 14:07:12 +0600

    merged

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import '@/styles/styles.scss'
+import '@/assets/styles/styles.scss'
 import App from '@/App.vue';
 
 const pinia = createPinia();
